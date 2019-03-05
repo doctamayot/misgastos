31bb4c2448a0ae74d9aa0ac6800201e5049438a1
doctamayot <doctamayot@hotmail.com>
2019-03-04 22:15:32 -0500

    heroku js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require rails-ujs
+//= require turbolinks
 //= require jquery
 //= require bootstrap-sprockets
-//= require turbolinks
 //= require Chart.bundle
 //= require chartkick
 //= require_tree .
