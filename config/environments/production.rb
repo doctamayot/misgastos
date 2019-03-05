7e62557ebb3005b3acc1317cadd3548674387f63
doctamayot <doctamayot@hotmail.com>
2019-03-04 20:58:03 -0500

    heroku js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
-  RAILS_ENV=production bundle exec rake assets:precompile
+  
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
